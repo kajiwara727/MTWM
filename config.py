53cef89ffb590a71d860e36165c2c640198f2458
YUUKAJI727\yuuka <yuukajiyan0727@gmail.com>
2025-10-13 15:52:07 +0900

    高速化等

--- config.py
+++ config.py
@@ -14,19 +14,19 @@ def get_targets_config():
     'factors': 混合の階層構造を定義する因子
     """
     return [
-        # {'name': 'Target 1', 'ratios': [45, 26, 64], 'factors': [3, 3, 3, 5]},
-        # {'name': 'Target 2', 'ratios': [80, 26, 29], 'factors': [3, 3, 3, 5]},
+        {'name': 'Target 1', 'ratios': [45, 26, 64], 'factors': [3, 3, 3, 5]},
+        {'name': 'Target 2', 'ratios': [80, 26, 29], 'factors': [3, 3, 3, 5]},
         # {'name': 'Target 1', 'ratios': [10, 55, 25], 'factors': [5, 3, 3, 2]},
-        {'name': 'Target 1', 'ratios': [2, 11, 5], 'factors': [3, 3, 2]},
-        {'name': 'Target 2', 'ratios': [12, 5, 1], 'factors': [3, 3, 2]},
+        # {'name': 'Target 1', 'ratios': [2, 11, 5], 'factors': [3, 3, 2]},
+        # {'name': 'Target 2', 'ratios': [12, 5, 1], 'factors': [3, 3, 2]},
         # {'name': 'Target 2', 'ratios': [60, 25, 5], 'factors': [5, 3, 3, 2]},
         # {'name': 'Target 3', 'ratios': [5, 6, 14], 'factors': [5, 5]},
-        # {'name': 'Target 4', 'ratios': [2, 11, 12], 'factors': [5, 5]},
+        # {'name': 'Target 4', 'ratios': [6, 33, 36], 'factors': [3, 5, 5]},
         
         # {'name': 'Target 1', 'ratios': [102, 26, 3, 3, 122], 'factors': [4, 4, 4, 4]},
         # {'name': 'Target 1', 'ratios': [2, 11, 5], 'factors': [3, 3, 2]},
         # {'name': 'Target 2', 'ratios': [12, 5, 1], 'factors': [3, 3, 2]},
-        # {'name': 'Target 3', 'ratios': [15, 18, 42], 'factors': [5, 3, 5]},
+        # {'name': 'Target 3', 'ratios': [5, 6, 14], 'factors': [5, 5]},
         
         # {'name': 'Target 1', 'ratios': [10, 55, 25], 'factors': [5, 3, 3, 2]},
         # {'name': 'Target 2', 'ratios': [60, 25, 5], 'factors': [5, 3, 3, 2]},
