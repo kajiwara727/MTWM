7634ff62340daebdc8afebb6777964afec76d505
YUUKAJI727\yuuka <yuukajiyan0727@gmail.com>
2025-10-20 09:12:02 +0900

    バグ修正中

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # 実行名を定義します。出力ディレクトリの名前の一部として使用されます。
-RUN_NAME = "18_90_25Random_Experiment_1"
+RUN_NAME = "multi-200-check"
 # 混合ツリーの階層構造（factors）を決定するモードを選択します。
 # 'manual': TARGETS_FOR_MANUAL_MODE で定義された factors を手動で設定します。
 # 'auto': 各ターゲットの ratios の合計値から factors を自動計算します。
 # 'auto_permutations': 'auto' で計算された factors の全順列を試し、最適な階層構造を探します。
 # 'random': RANDOM_SETTINGS に基づいてランダムなシナリオを複数回実行します。
-FACTOR_EXECUTION_MODE = "random"
+FACTOR_EXECUTION_MODE = "auto"
 # 最適化の目的を設定します。
 # "waste": 廃棄物量の最小化を目指します。
 # "operations": 混合操作の総回数の最小化を目指します。
@@ -16,7 +16,7 @@ ENABLE_CHECKPOINTING = False
 # --- 制約条件 ---
 
 # ノード間で共有（中間液を融通）できる液量の最大値を設定します。Noneの場合は無制限です。
-MAX_SHARING_VOLUME = 4
+MAX_SHARING_VOLUME = 3
 # 中間液を共有する際の、供給元と供給先の階層レベル（level）の差の最大値を設定します。Noneの場合は無制限です。
 MAX_LEVEL_DIFF = None
 # 1回の混合操作で使用できるミキサーの最大容量（入力の合計値）を設定します。これはDFMMアルゴリズムで混合ツリーの階層を決定する際の因数の最大値にもなります。
@@ -32,7 +32,7 @@ RANDOM_SETTINGS = {
     # --- 混合比和の生成ルール（以下の優先順位で適用されます） ---
     # 優先度1: 固定シーケンス（n_targetsと要素数を一致させる必要あり）
     # 18*5' と書くと「合計18の混合比を生成し、各要素を5倍する」というロジックが適用
-    # 例： 'S_ratio_sum_sequence': [18, 18*5, 25]
+    # 例： 'S_ratio_sum_sequence': [18, {'base_sum': 18, 'multiplier': 5},, 25]
     'S_ratio_sum_sequence': [
         18,
         {'base_sum': 18, 'multiplier': 5},
@@ -51,8 +51,14 @@ RANDOM_SETTINGS = {
 # FACTOR_EXECUTION_MODE が 'auto' または 'auto_permutations' の場合に使用されます。
 # 'factors' を指定する必要はありません。自動で計算されます。
 TARGETS_FOR_AUTO_MODE = [
-    {'name': 'Target 1', 'ratios': [2, 11, 5]},
-    {'name': 'Target 2', 'ratios': [12, 5, 1]},
+    {'name': 'Target 1', 'ratios': [97, 97, 6]},
+    {'name': 'Target 1', 'ratios': [97, 97, 6]},
+    # {'name': 'Target 1', 'ratios': [97, 97, 6]},
+    # {'name': 'Target 1', 'ratios': [14, 10, 1]},
+    # {'name': 'Target 2', 'ratios': [7, 8, 10]},
+    # {'name': 'Target 3', 'ratios': [5, 12, 8]},
+    # {'name': 'Target 1', 'ratios': [2, 11, 5]},
+    # {'name': 'Target 2', 'ratios': [12, 5, 1]},
     # {'name': 'Target 3', 'ratios': [5, 6, 14]},
     # {'name': 'Target 1', 'ratios': [45, 26, 64]},
     # {'name': 'Target 1', 'ratios': [2, 11, 5]},
@@ -74,20 +80,20 @@ TARGETS_FOR_MANUAL_MODE = [
     # {'name': 'Target 1', 'ratios': [45, 26, 64], 'factors': [3, 3, 3, 5]},
     # {'name': 'Target 2', 'ratios': [80, 26, 29], 'factors': [3, 3, 3, 5]},
     # {'name': 'Target 1', 'ratios': [10, 55, 25], 'factors': [3, 5, 3, 2]},
-    {'name': 'Target 1', 'ratios': [6, 33, 15], 'factors': [3, 3, 3, 2]},
+    # {'name': 'Target 1', 'ratios': [10, 55, 25], 'factors': [3, 5, 3, 2]},
     # {'name': 'Target 1', 'ratios': [2, 11, 5], 'factors': [3, 3, 2]},
-    {'name': 'Target 2', 'ratios': [12, 5, 1], 'factors': [3, 3, 2]},
+    # {'name': 'Target 2', 'ratios': [25, 60, 5], 'factors': [3, 5, 3, 2]},
     # {'name': 'Target 3', 'ratios': [4, 5, 9], 'factors': [3, 3, 2]},
     # {'name': 'Target 3', 'ratios': [3, 5, 10], 'factors': [3, 3, 2]},
     # {'name': 'Target 4', 'ratios': [7, 7, 4], 'factors': [3, 3, 2]},
     # {'name': 'Target 2', 'ratios': [60, 25, 5], 'factors': [5, 3, 3, 2]},
-    {'name': 'Target 3', 'ratios': [5, 6, 14], 'factors': [5, 5]},
+    # {'name': 'Target 3', 'ratios': [5, 6, 14], 'factors': [5, 5]},
     # {'name': 'Target 4', 'ratios': [6, 33, 36], 'factors': [3, 5, 5]},
 
     # {'name': 'Target 1', 'ratios': [102, 26, 3, 3, 122], 'factors': [4, 4, 4, 4]},
-    # {'name': 'Target 1', 'ratios': [2, 11, 5], 'factors': [3, 3, 2]},
-    # {'name': 'Target 2', 'ratios': [12, 5, 1], 'factors': [3, 3, 2]},
-    # {'name': 'Target 3', 'ratios': [5, 6, 14], 'factors': [5, 5]},
+    {'name': 'Target 1', 'ratios': [2, 11, 5], 'factors': [3, 3, 2]},
+    {'name': 'Target 2', 'ratios': [12, 5, 1], 'factors': [3, 3, 2]},
+    {'name': 'Target 3', 'ratios': [6, 5, 7], 'factors': [3, 3, 2]},
 
     # {'name': 'Target 1', 'ratios': [10, 55, 25], 'factors': [5, 3, 3, 2]},
     # {'name': 'Target 2', 'ratios': [60, 25, 5], 'factors': [5, 3, 3, 2]},
